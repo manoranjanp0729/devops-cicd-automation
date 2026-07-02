a6e93697dba7d2c804b360f565638c43a9f9c73f
Manoranjan <manoranjanp0729@gmail.com>
2026-07-02 10:58:06 +0000

    Complete end-to-end DevOps CI/CD project

---
+++ app/src/main/java/com/manoranjan/cicd_demo/HomeController.java
@@ -0,0 +1,13 @@
+package com.manoranjan.cicd_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from DevOps CI/CD Project - Version 1.0";
+    }
+}
